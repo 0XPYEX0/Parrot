da3f67ddb3b2b0bc8c2edd549729dc12865c88f1
XPYEX <XPYEX0@163.com>
2022-08-09 13:12:02 +0800

    新增点歌针对群限制

--- src/main/java/me/xpyex/plugin/allinone/model/Music.java
+++ src/main/java/me/xpyex/plugin/allinone/model/Music.java
@@ -5,6 +5,7 @@ import java.net.HttpURLConnection;
 import java.net.URLEncoder;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -19,6 +20,7 @@ import me.xpyex.plugin.allinone.modelcode.music.musicsource.KugouMusicSource;
 import me.xpyex.plugin.allinone.modelcode.music.musicsource.NetEaseMusicSource;
 import me.xpyex.plugin.allinone.modelcode.music.musicsource.QQMusicSource;
 import me.xpyex.plugin.allinone.utils.Util;
+import net.mamoe.mirai.event.events.GroupMessageEvent;
 import net.mamoe.mirai.event.events.MessageEvent;
 
 @SuppressWarnings("unused")
@@ -27,6 +29,8 @@ public class Music extends Model {
     public static final Map<String, BiConsumer<MessageEvent, String[]>> MUSIC_CMDS = new ConcurrentHashMap<>();
     private static final Map<String, MusicSource> SOURCES = Collections.synchronizedMap(new LinkedHashMap<>());
     private static final Map<String, MusicCardProvider> CARDS = new ConcurrentHashMap<>();
+    private static final HashMap<Long, Integer> GROUP_LIMITER = new HashMap<>();
+
     static {
         SOURCES.put("QQ音乐", new QQMusicSource());
         SOURCES.put("网易", new NetEaseMusicSource());
@@ -36,6 +40,8 @@ public class Music extends Model {
         MUSIC_CMDS.put("#点歌", makeTemplate("QQ音乐", "MiraiCard"));
         MUSIC_CMDS.put("#网易", makeTemplate("网易", "MiraiCard"));
         MUSIC_CMDS.put("#酷狗", makeTemplate("酷狗", "MiraiCard"));
+
+        GROUP_LIMITER.put(741053728L, 0);
     }
 
     @Override
@@ -46,6 +52,9 @@ public class Music extends Model {
             if (exec != null)
                 exec.accept(event, args);
         });
+        runTaskTimer(() -> {
+            GROUP_LIMITER.replaceAll((k, v) -> 0);
+        }, 60 * 60, 60 * 60);
     }
 
     public static BiConsumer<MessageEvent, String[]> makeTemplate(String source, String card) {
@@ -56,6 +65,10 @@ public class Music extends Model {
         if (mc == null)
             throw new IllegalArgumentException("music source not exists");
         return (event, args) -> {
+            if (Util.isGroupEvent(event) && GROUP_LIMITER.containsKey(((GroupMessageEvent) event).getGroup().getId()) && GROUP_LIMITER.get(((GroupMessageEvent) event).getGroup().getId()) > 20) {
+                Util.autoSendMsg(event, "抱歉，根据管理员设定，该群每个小时仅允许点歌20首\n当前已超出限制");
+                return;
+            }
             String sn;
             try {
                 sn = URLEncoder.encode(String.join(" ", Arrays.copyOfRange(args, 1, args.length)), "UTF-8");
