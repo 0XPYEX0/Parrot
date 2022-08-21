c5633f09ccd9759a788c65d21c4b9d3e82baff53
XPYEX <XPYEX0@163.com>
2022-08-22 02:35:20 +0800

    合并上游，修复QQ点歌

--- src/main/java/me/xpyex/plugin/allinone/modelcode/music/MusicUtils.java
+++ src/main/java/me/xpyex/plugin/allinone/modelcode/music/MusicUtils.java
@@ -1,14 +1,44 @@
 package me.xpyex.plugin.allinone.modelcode.music;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Date;
 import me.xpyex.plugin.allinone.utils.Util;
 
 public final class MusicUtils {
+    
+    /**
+     * Read all content from input stream.<br>
+     * 从数据流读取全部数据
+     *
+     * @param i the input stream<br>
+     *          数据流
+     * @return return all read data <br>
+     *         返回读入的所有数据
+     * @throws IOException Signals that an I/O exception has occurred.<br>
+     *                     发生IO错误
+     */
+    public static byte[] readAll(InputStream i) throws IOException {
+        ByteArrayOutputStream ba = new ByteArrayOutputStream(16384);
+        int nRead;
+        byte[] data = new byte[4096];
+        
+        try {
+            while ((nRead = i.read(data, 0, data.length)) != -1) {
+                ba.write(data, 0, nRead);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw e;
+        }
+        
+        return ba.toByteArray();
+    }
 
     public static byte[] readAll(final File i) {
         try (final FileInputStream fis = new FileInputStream(i)) {
--- src/main/java/me/xpyex/plugin/allinone/modelcode/music/musicsource/QQMusicSource.java
+++ src/main/java/me/xpyex/plugin/allinone/modelcode/music/musicsource/QQMusicSource.java
@@ -11,7 +11,6 @@ import java.nio.charset.StandardCharsets;
 import me.xpyex.plugin.allinone.modelcode.music.MusicInfo;
 import me.xpyex.plugin.allinone.modelcode.music.MusicSource;
 import me.xpyex.plugin.allinone.modelcode.music.MusicUtils;
-import me.xpyex.plugin.allinone.utils.Util;
 
 public class QQMusicSource implements MusicSource {
 
@@ -20,22 +19,10 @@ public class QQMusicSource implements MusicSource {
 
     public String queryRealUrl(String songmid) {
         try {
-            StringBuilder urlsb = new StringBuilder(
-                    "https://u.y.qq.com/cgi-bin/musicu.fcg?format=json&data=%7B%22req_0%22%3A%7B%22module%22%3A%22vkey.GetVkeyServer%22%2C%22method%22%3A%22CgiGetVkey%22%2C%22param%22%3A%7B%22guid%22%3A%22358840384%22%2C%22songmid%22%3A%5B%22");
+            StringBuilder urlsb = new StringBuilder("https://u.y.qq.com/cgi-bin/musicu.fcg?format=json&data=%7B%22req_0%22%3A%7B%22module%22%3A%22vkey.GetVkeyServer%22%2C%22method%22%3A%22CgiGetVkey%22%2C%22param%22%3A%7B%22guid%22%3A%22358840384%22%2C%22songmid%22%3A%5B%22");
             urlsb.append(songmid);
-            urlsb.append(
-                    "%22%5D%2C%22songtype%22%3A%5B0%5D%2C%22uin%22%3A%221443481947%22%2C%22loginflag%22%3A1%2C%22platform%22%3A%2220%22%7D%7D%2C%22comm%22%3A%7B%22uin%22%3A%2218585073516%22%2C%22format%22%3A%22json%22%2C%22ct%22%3A24%2C%22cv%22%3A0%7D%7D");
-            /*
-            URL u = new URL(urlsb.toString());
-            HttpURLConnection conn = (HttpURLConnection) u.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Host", "u.y.qq.com");
-            conn.setRequestProperty("User-Agent",
-                    "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1");
-            conn.connect();
-            byte[] bs = MusicUtils.readAll(conn.getInputStream());
-             */
-
+            urlsb.append("%22%5D%2C%22songtype%22%3A%5B0%5D%2C%22uin%22%3A%221443481947%22%2C%22loginflag%22%3A1%2C%22platform%22%3A%2220%22%7D%7D%2C%22comm%22%3A%7B%22uin%22%3A%2218585073516%22%2C%22format%22%3A%22json%22%2C%22ct%22%3A24%2C%22cv%22%3A0%7D%7D");
+            
             JSONObject out = JSONUtil.parseObj(HttpUtil.get(urlsb.toString()));
             if (out.getInt("code") != 0) {
                 return null;
@@ -54,22 +41,39 @@ public class QQMusicSource implements MusicSource {
 
     @Override
     public MusicInfo get(String keyword) throws Exception {
-        URL url = new URL("https://c.y.qq.com/soso/fcgi-bin/client_search_cp?p=1&cr=1&aggr=1&flag_qc=0&n=3&w=" + keyword
-                + "&format=json");
+        URL url = new URL("https://u.y.qq.com/cgi-bin/musicu.fcg");
         HttpURLConnection huc = (HttpURLConnection) url.openConnection();
         huc.setRequestProperty("User-Agent",
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36");
-        huc.setRequestMethod("GET");
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36");
+    
+        JSONObject scs = new JSONObject();
+        JSONObject search = new JSONObject();
+        JSONObject searchParam = new JSONObject();
+        searchParam.set("query", keyword);
+        searchParam.set("num_per_page",3);
+        searchParam.set("search_type",0);
+        searchParam.set("page_num",1);
+        search.set("param",searchParam);
+        search.set("module","music.search.SearchCgiService");
+        search.set("method", "DoSearchForQQMusicDesktop");
+        scs.set("music.search.SearchCgiService", search);
+        huc.setRequestMethod("POST");
+        huc.setRequestProperty("referer","https://y.qq.com");
+        huc.setDoOutput(true);
         huc.connect();
-        JSONArray ss = JSONUtil.parseObj(new String(Util.readAll(huc.getInputStream()), StandardCharsets.UTF_8))
-                .getJSONObject("data").getJSONObject("song").getJSONArray("list");
+        huc.getOutputStream().write(scs.toString().getBytes(StandardCharsets.UTF_8));
+        String s = new String(MusicUtils.readAll(huc.getInputStream()), StandardCharsets.UTF_8);
+        s = s.substring(s.indexOf("{"));
+        s = s.substring(0,s.lastIndexOf("}") + 1);
+        JSONArray ss = new JSONObject(s).getJSONObject("music.search.SearchCgiService")
+                           .getJSONObject("data").getJSONObject("body").getJSONObject("song").getJSONArray("list");
         JSONObject song = ss.getJSONObject(0);// .data.song.list
-        String mid = song.getStr("songmid");
+        String mid = song.getStr("mid");
         String musicURL = queryRealUrl(mid);
         int i = 0;
         while (!MusicUtils.isExistent(musicURL)) {
             song = ss.getJSONObject(++i);
-            mid = song.getStr("songmid");
+            mid = song.getStr("mid");
             musicURL = queryRealUrl(mid);
         }
         String desc;
@@ -77,25 +81,23 @@ public class QQMusicSource implements MusicSource {
             JSONArray singers = song.getJSONArray("singer");
             StringBuilder sgs = new StringBuilder();
             for (Object je : singers) {
-                if (je instanceof JSONObject) {
-                    sgs.append(((JSONObject) je).getStr("name"));
-                    sgs.append(";");
-                }
+                sgs.append(new JSONObject(je).getStr("name"));
+                sgs.append(";");
             }
             sgs.deleteCharAt(sgs.length() - 1);
             desc = sgs.toString();
         } catch (Exception e) {
-            desc = song.getStr("albumname");
+            desc = song.getJSONObject("album").getStr("name");
         }
-
+    
         if (musicURL == null) {
             throw new FileNotFoundException();
         }
-        return new MusicInfo(song.getStr("songname"), desc,
-                "http://y.gtimg.cn/music/photo_new/T002R300x300M000" + song.getStr("albummid") + ".jpg",
-                musicURL, "https://i.y.qq.com/v8/playsong.html?_wv=1&songid=" + song.getStr("songid")
-                + "&source=qqshare&ADTAG=qqshare",
-                "QQ音乐", "https://url.cn/PwqZ4Jpi", 100497308);
+        return new MusicInfo(song.getStr("title"), desc,
+            "http://y.gtimg.cn/music/photo_new/T002R300x300M000" + song.getJSONObject("album").getStr("mid") + ".jpg",
+            musicURL, "https://i.y.qq.com/v8/playsong.html?_wv=1&songid=" + song.getStr("id")
+                          + "&source=qqshare&ADTAG=qqshare",
+            "QQ音乐", "https://url.cn/PwqZ4Jpi", 100497308);
     }
 
 }
