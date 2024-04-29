c92a0cf69e50d65873b03442cb2391456bfb249a
XPYEX <XPYEX0@163.com>
2024-04-29 22:33:44 +0800

    优化代码

--- src/main/java/me/xpyex/plugin/allinone/module/StaffTeam.java
+++ src/main/java/me/xpyex/plugin/allinone/module/StaffTeam.java
@@ -1,8 +1,9 @@
 package me.xpyex.plugin.allinone.module;
 
 import me.xpyex.plugin.allinone.api.CommandMenu;
+import me.xpyex.plugin.allinone.core.command.argument.ArgParser;
+import me.xpyex.plugin.allinone.core.command.argument.UserParser;
 import me.xpyex.plugin.allinone.core.module.Module;
-import me.xpyex.plugin.allinone.utils.MsgUtil;
 import net.mamoe.mirai.contact.Group;
 import net.mamoe.mirai.contact.MemberPermission;
 import net.mamoe.mirai.contact.NormalMember;
@@ -13,41 +14,32 @@ public class StaffTeam extends Module {
     public void register() {
         registerCommand(Group.class, ((source, sender, label, args) -> {
             if (source.getContact().getBotPermission() != MemberPermission.OWNER) {
-                MsgUtil.sendMsg(source, "Bot非群主，无法执行此操作");
+                source.sendMessage("Bot非群主，无法执行此操作");
                 return;
             }
-            if (!sender.hasPerm(getName() + ".use", MemberPermission.ADMINISTRATOR)) {
-                if (args.length == 0) {
-                    new CommandMenu(label)
-                        .add("add <QQID>", "令群员成为管理员")
-                        .add("remove <QQID>", "令管理员成为普通群员")
-                        .send(source);
-                } else if (args.length == 1) {
-                    MsgUtil.sendMsg(source, "参数不足");
-                } else {
-                    try {
-                        long targetID = Long.parseLong(args[1]);
-                        NormalMember target = source.getContact().get(targetID);
-                        if (target == null) {
-                            MsgUtil.sendMsg(source, "群内无该成员");
-                            return;
-                        }
-                        if (args[0].equalsIgnoreCase("add")) {
-                            target.modifyAdmin(true);
-                        } else if (args[0].equalsIgnoreCase("remove")) {
-                            target.modifyAdmin(false);
-                        } else {
-                            MsgUtil.sendMsg(source, "未知子命令");
-                            return;
-                        }
-                        MsgUtil.sendMsg(source, "已完成");
-                    } catch (NumberFormatException ignored) {
-                        MsgUtil.sendMsg(source, "请输入正确的QQ号");
-                    }
-                }
-            } else {
-                MsgUtil.sendMsg(source, "你没有权限");
+            if (!sender.hasPerm(getName() + ".use")) {
+                source.sendMessage("你没有权限");
+                return;
             }
-        }), "staff", "StaffTeam");
+            if (args.length == 0) {
+                new CommandMenu(label)
+                    .add("add <QQID>", "令群员成为管理员")
+                    .add("remove <QQID>", "令管理员成为普通群员")
+                    .send(source);
+                return;
+            }
+            ArgParser.of(UserParser.class).parse(() -> args[1], NormalMember.class)  //命令为群命令，只打算拿到Member
+                .ifPresentOrElse(member -> {
+                    if ("add".equalsIgnoreCase(args[0])) {
+                        member.modifyAdmin(true);
+                    } else if ("remove".equalsIgnoreCase(args[0])) {
+                        member.modifyAdmin(false);
+                    } else {
+                        source.sendMessage("未知子命令");
+                        return;
+                    }
+                    source.sendMessage("已完成");
+                }, () -> source.sendMessage("群内无该成员"));
+        }), "staff", "StaffTeam", "admin", "administrator");
     }
 }
