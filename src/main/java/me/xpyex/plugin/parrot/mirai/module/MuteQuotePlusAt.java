b26e5c1c6a7fcab331f729d60023ef38b0bffcbd
XPYEX <XPYEX0@163.com>
2024-09-21 13:05:33 +0800

    perf: 上调至30s

--- src/main/java/me/xpyex/plugin/parrot/mirai/module/MuteQuotePlusAt.java
+++ src/main/java/me/xpyex/plugin/parrot/mirai/module/MuteQuotePlusAt.java
@@ -2,7 +2,6 @@ package me.xpyex.plugin.parrot.mirai.module;
 
 import me.xpyex.plugin.parrot.mirai.core.module.Module;
 import me.xpyex.plugin.parrot.mirai.module.core.PermManager;
-import net.mamoe.mirai.contact.MemberPermission;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 import net.mamoe.mirai.message.data.At;
 import net.mamoe.mirai.message.data.ForwardMessageBuilder;
@@ -12,7 +11,6 @@ import net.mamoe.mirai.message.data.QuoteReply;
 public class MuteQuotePlusAt extends Module {
     @Override
     public void register() throws Throwable {
-        this.DEFAULT_DISABLED = true;
         listenEvent(GroupMessageEvent.class, event -> {
             if (event.getGroup().getBotPermission().getLevel() > event.getPermission().getLevel()) {  //Bot权限高于Sender
                 QuoteReply quote = event.getMessage().get(QuoteReply.Key);
@@ -20,10 +18,10 @@ public class MuteQuotePlusAt extends Module {
                     if (event.getMessage().get(At.Key) != null) {  //消息有At
                         if (!event.getMessage().contentToString().contains("@" + quote.getSource().getFromId()))
                             return;  //如果回复时@的不是消息的发送者，那可能是刻意在@其他人，不应处理
-                        if (PermManager.hasPerm(event.getSender(), getName() + ".bypass", MemberPermission.ADMINISTRATOR))
+                        if (PermManager.hasPerm(event.getSender(), getName() + ".bypass", null))
                             return;
 
-                        event.getSender().mute(10);  //10s
+                        event.getSender().mute(30);  //30s
                         event.getGroup().sendMessage("能不能回复的时候不@人啊你妈的");
                         recall(event.getSource());  //撤回
                         event.getGroup().sendMessage(
